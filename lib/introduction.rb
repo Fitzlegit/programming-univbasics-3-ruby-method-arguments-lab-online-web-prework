36005c6192d0e089ba8f2065bd946ecdce748f83
Fitzhugh <j.rfitzhugh@gmail.com>
2019-10-15 00:53:40 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,11 +7,11 @@ def introduction(name)
 end
 
 def introduction_with_language(name, language)
-  puts "Hi my name is #{name} and I am learning to program #{language}."
+  puts "Hi, my name is #{name} and I am learning to program #{language}."
 end
 
 def introduction_with_language(name, language = 'Ruby')
-  puts "Hi my name is #{name} and I am learning to program #{language}."
+  puts "Hi, my name is #{name} and I am learning to program #{language}."
 end
 
 
